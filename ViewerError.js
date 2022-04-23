40054e210b92c93824b38312ec1f789c2e2d83e7
Camila Figueiredo <camiladiasfigueiredo@gmail.com>
2022-04-22 22:51:39 -0300

    clean project

--- ViewerError.js
+++
@@ -1,9 +0,0 @@
-
-export default class ViewerError extends Error {
-  
-    // Construtor da Classe ViewerError
-    constructor(txtDeErro) {
-      super(txtDeErro); // Chamando o construtor da superclasse (Error)
-      console.log(txtDeErro + '\n\n' + this.stack);
-    }
-}
